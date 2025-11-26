c36ec025b2e33ee9876affc5600caee07a702420
Ivan Zayashnikov <vanyazaov@gmail.com>
2025-11-26 22:28:28 +0300

    Создание Repository Interface (контракт доменного слоя)

--- reviews.list/lib/Domain/Repository/ReviewRepositoryInterface.php
+++ reviews.list/lib/Domain/Repository/ReviewRepositoryInterface.php
@@ -1,1 +1,21 @@
 <?php
+namespace Custom\Reviews\Component\Lib\Domain\Repository;
+
+use Custom\Reviews\Component\Lib\Domain\Entity\Review;
+use Custom\Reviews\Component\Lib\Domain\ValueObject\ReviewId;
+
+interface ReviewRepositoryInterface
+{
+    public function save(Review $review): void;
+    
+    public function findById(ReviewId $id): ?Review;
+    
+    /**
+     * @return Review[]
+     */
+    public function findAll(int $limit = 10, int $offset = 0): array;
+    
+    public function countAll(): int;
+    
+    public function getLatest(int $count = 10): array;
+}

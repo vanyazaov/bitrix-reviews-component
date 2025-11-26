1ec12fb2453c976506031f0fd03ff850515a07e8
Ivan Zayashnikov <vanyazaov@gmail.com>
2025-11-26 22:39:28 +0300

    Создание основных файлов компонента

---
+++ reviews.list/.description.php
@@ -0,0 +1,12 @@
+<?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$arComponentDescription = [
+    "NAME" => "Список отзывов (DDD)",
+    "DESCRIPTION" => "Компонент вывода отзывов с DDD архитектурой",
+    "PATH" => [
+        "ID" => "custom",
+        "NAME" => "Кастомные компоненты",
+    ],
+    "ICON" => "/images/icon.gif",
+];
--- reviews.list/.parameters.php
+++ reviews.list/.parameters.php
@@ -1,1 +1,16 @@
 <?php
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
+
+$arComponentParameters = [
+    "PARAMETERS" => [
+        "COUNT" => [
+            "PARENT" => "BASE",
+            "NAME" => "Количество отзывов",
+            "TYPE" => "STRING",
+            "DEFAULT" => "10",
+        ],
+        "CACHE_TIME" => [
+            "DEFAULT" => 300,
+        ],
+    ],
+];
